8fb1ec957b7344ada5def5ca07bd95695f94f0a3
gratusrichard <gratusri@gmail.com>
2025-05-15 22:02:08 +0100

    minor typo

---
+++ docs/data/generated/bib.js
@@ -0,0 +1,106 @@
+﻿const generatedBibEntries = {
+    "Anagnostopoulos2023": {
+        "author": "Anagnostopoulos, A. and Griffiths, B.E. and Siachos, Nektarios and Neary, J. and Smith, R.F. and Oikonomou, Georgios",
+        "doi": "10.3389/fvets.2023.1111057",
+        "journal": "Frontiers in Veterinary Science",
+        "title": "Initial validation of an intelligent video surveillance system for automatic detection of dairy cattle lameness",
+        "type": "article",
+        "volume": "10",
+        "year": "2023"
+    },
+    "Bergamini2021": {
+        "author": "Bergamini, L. and Porrello, A. and Dondona, A.C. and Del Negro, E. and Mattioli, M. and D\u2019alterio, N. and Calderara, S.",
+        "booktitle": "2018 14th International Conference on Signal-Image Technology \\& Internet-Based Systems (SITIS)",
+        "doi": "10.1109/sitis.2018.00036",
+        "pages": "184--191",
+        "title": "Multi-views Embedding for Cattle Re-identification",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "Bergman2024": {
+        "author": "Bergman, N. and Yitzhaky, Y. and Halachmi, I.",
+        "doi": "10.1016/j.animal.2024.101079",
+        "journal": "Animal",
+        "number": "3",
+        "pages": "101079",
+        "title": "Biometric identification of dairy cows via real-time facial recognition",
+        "type": "article",
+        "volume": "18",
+        "year": "2024"
+    },
+    "BorgesOliveira2021": {
+        "author": "Borges Oliveira, D.A. and Ribeiro Pereira, L.G. and Bresolin, T. and Pontes Ferreira, R.E. and Reboucas Dorea, J.R.",
+        "doi": "10.1016/j.livsci.2021.104700",
+        "journal": "Livestock Science",
+        "pages": "104700",
+        "title": "A review of deep learning algorithms for computer vision systems in livestock",
+        "type": "article",
+        "volume": "253",
+        "year": "2021"
+    },
+    "Carslake2020": {
+        "author": "Carslake, C. and V\u00e1zquez-Diosdado, J.A. and Kaler, J.",
+        "doi": "10.3390/s21010088",
+        "journal": "Sensors",
+        "number": "1",
+        "pages": "88",
+        "title": "Machine Learning Algorithms to Classify and Quantify Multiple Behaviours in Dairy Calves Using a Sensor: Moving beyond Classification in Precision Livestock",
+        "type": "article",
+        "volume": "21",
+        "year": "2020"
+    },
+    "Gusterer2020": {
+        "author": "Gusterer, E. and Kanz, P. and Krieger, S. and Schweinzer, V. and S\u00fcss, D. and Lidauer, L. and Kickinger, F. and \u00d6hlschuster, M. and Auer, W. and Drillich, M. and Iwersen, M.",
+        "doi": "10.1016/j.theriogenology.2020.07.028",
+        "journal": "Theriogenology",
+        "pages": "61--69",
+        "title": "Sensor technology to support herd health monitoring: Using rumination duration and activity measures as unspecific variables for the early detection of dairy cows with health deviations",
+        "type": "article",
+        "volume": "157",
+        "year": "2020"
+    },
+    "Hudson2018": {
+        "author": "Hudson, C. and Kaler, J. and Down, P.",
+        "doi": "10.1136/inp.k4328",
+        "journal": "In Practice",
+        "number": "9",
+        "pages": "396--410",
+        "title": "Using big data in cattle practice",
+        "type": "article",
+        "volume": "40",
+        "year": "2018"
+    },
+    "Hyde2021": {
+        "author": "Hyde, R. and Green, M. and Hudson, C. and Remnant, J.",
+        "doi": "10.1002/inpr.53",
+        "journal": "In Practice",
+        "number": "5",
+        "pages": "257--268",
+        "title": "Practical data skills for the farm animal vet",
+        "type": "article",
+        "volume": "43",
+        "year": "2021"
+    },
+    "Hyde2023": {
+        "author": "Hyde, R. and Carslake, C.",
+        "doi": "10.12968/live.2023.28.6.254",
+        "journal": "Livestock",
+        "number": "6",
+        "pages": "254--256",
+        "title": "The future of AI for livestock veterinary practice",
+        "type": "article",
+        "volume": "28",
+        "year": "2023"
+    },
+    "Norouzzadeh2018": {
+        "author": "Norouzzadeh, M.S. and Nguyen, A. and Kosmala, M. and Swanson, A. and Palmer, M.S. and Packer, C. and Clune, J.",
+        "doi": "10.1073/pnas.1719367115",
+        "journal": "Proceedings of the National Academy of Sciences",
+        "number": "25",
+        "pages": "E5716--E5725",
+        "title": "Automatically identifying, counting, and describing wild animals in camera-trap images with deep learning",
+        "type": "article",
+        "volume": "115",
+        "year": "2018"
+    }
+};
